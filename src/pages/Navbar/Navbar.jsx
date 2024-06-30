96865473dd002ef572ec01c318f45e259cc6d1dd
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-07-01 00:07:27 +0600

    navbar photo and hover name added

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import 'animate.css';
 import { useContext } from "react";
 import { AuthContext } from "../../providers/Authproviders";
 const Navbar = () => {
-    const {logOut,user}=useContext(AuthContext);
-    const handleLogout = ()=>{
+    const { logOut, user } = useContext(AuthContext);
+    const handleLogout = () => {
         logOut()
-        .then()
-        .catch()
+            .then()
+            .catch()
     }
     const navLinks = <>
 
@@ -18,7 +18,7 @@ const Navbar = () => {
             <li><NavLink className='btn  bg-transparent font-semibold text-base border-none' to="/about">About</NavLink></li>
             <li><NavLink className='btn  bg-transparent font-semibold text-base border-none' to="/register">Register</NavLink></li>
         </div>
-        
+
 
 
     </>
@@ -30,12 +30,12 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 navigation">
-                       {user?    
-                        <li className="btn bg-transparent font-semibold text-base  border-none md:hidden">{user.displayName}</li>
-                    : 
-                    <li ><NavLink className='btn bg-transparent font-semibold text-base  border-none' to="/login">Login</NavLink></li>
-                    
-                    }
+                        {user ?
+                            <li className="btn bg-transparent font-semibold text-base  border-none md:hidden">{user.displayName}</li>
+                            :
+                            <li ><NavLink className='btn bg-transparent font-semibold text-base  border-none' to="/login">Login</NavLink></li>
+
+                        }
                         {navLinks}
                     </ul>
                 </div>
@@ -47,24 +47,29 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                {user?   
-                
-                <>
+                {user ?
 
-                <h4 className="mr-4 font-semibold hidden md:block">
-                    User: <span>{user.displayName}</span>
-                </h4>
-                 <Link onClick={handleLogout}>
-                 <a className="btn text-white bg-[#5C2751] border-none animate__animated animate__zoomIn ">Log Out</a></Link>
-                
-                </>
-                :   
-                
-                <Link to='/login'>
-               <a className="btn text-white bg-[#5C2751] border-none animate__animated animate__zoomIn ">Log in</a></Link>
-                
+                    <>
+
+                        <div className="avatar online mr-2">
+                            <div className="w-10 md:w-14 rounded-full group">
+                                <img src={user.photoURL} />
+                                <div className="absolute top-20 md:top-24 inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                                    <div className="text-[#5C2751] font-bold text-xl">{user.displayName}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <Link onClick={handleLogout}>
+                            <a className="btn text-white bg-[#5C2751] border-none animate__animated animate__zoomIn ">Log Out</a></Link>
+
+                    </>
+                    :
+
+                    <Link to='/login'>
+                        <a className="btn text-white bg-[#5C2751] border-none animate__animated animate__zoomIn ">Log in</a></Link>
+
                 }
-               
+
             </div>
         </div>
     );
