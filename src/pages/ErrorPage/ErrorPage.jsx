13ffc052d8bbb3d25be849a673e0b656545b2350
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-07-01 12:02:58 +0600

    password requirements added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -7,8 +7,8 @@ const ErrorPage = () => {
            
            <div className="flex flex-col justify-center items-center   h-screen gap-5"> 
            <img className='mx-auto  w-1/4' src={builder} alt="" />
-           <h2 className="text-red-600 font-bold text-3xl text-center flex flex-col items-center ">404 Not Found! </h2>
-           <h2 className="text-red-600 font-bold text-3xl text-center flex flex-col items-center ">This Page is Still Under Construction!!! <br /> <p className='text-purple-500'>Checkout our other pages! <FaRegSmileBeam className="inline-block" />
+           <h2 className="text-red-600 font-bold text-5xl text-center flex flex-col items-center ">404 Not Found! </h2>
+           <h2 className="text-red-600 font-bold text-3xl text-center flex flex-col items-center ">This Page is Still Under Construction or does not exist at the moment!!! <br /> <p className='text-purple-500'>Please checkout our other pages! <FaRegSmileBeam className="inline-block" />
            
            </p>
            </h2>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,82 +5,116 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
-const {createUser,updateUserProfile}=useContext(AuthContext);
-const notifyLogin = () => {
-    toast.success('Successfully Registered', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const notifyLogin = () => {
+        toast.success('Successfully Registered', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
 
-    });
-}
-const handleRegister = (e) => {
-    e.preventDefault();
-    const form = new FormData(e.currentTarget);
-    const email = form.get('email');
-    const name = form.get('name');
-    const photo = form.get('photo');
-    const password = form.get('password');
-    console.log(email, name, photo, password);
-
-    createUser(email, password)
-        .then((result) => {
-            console.log(result.user);
-            notifyLogin();
-            return updateUserProfile(name, photo);
-           
-        })
-        .catch((error) => {
-            console.error(error);
         });
-};
+    }
+    const notifyPassword = message => {
+        toast.warn(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            // transition: Flip,
+        });
+    }
+    const validPassword = password => {
+        const hasUpperCase = /[A-Z]/.test(password);
+        const hasLowerCase = /[a-z]/.test(password);
+        const isValidLength = password.length >= 6;
+        if (!hasUpperCase) {
+            notifyPassword('Password must have at least one uppercase letter');
+            return false;
+        }
+        if (!hasLowerCase) {
+            notifyPassword('Password must have at least one lowercase letter');
+            return false;
+        }
+        if (!isValidLength) {
+            notifyPassword('Password should be at least 6 character long');
+            return false;
+        }
+        return true;
+    }
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const password = form.get('password');
+        if(!validPassword(password)){
+                return;
+        }
+        console.log(email, name, photo, password);
+
+        createUser(email, password)
+            .then((result) => {
+                console.log(result.user);
+                notifyLogin();
+                return updateUserProfile(name, photo);
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
     return (
 
-    
-
-            <div className="">
-                <h3 className="text-3xl font-bold text-center mt-6">Please Register</h3>
-                <form onSubmit={handleRegister} className="card-body md:w-3/4 lg:1/2 mx-auto">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Photo URL</span>
-                        </label>
-                        <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
-                        
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn text-white text-xl border-none bg-[#5C2751]">Create Account</button>
-                    </div>
-                </form>
-                <p className="text-center font-semibold text-lg mt-4">Already have an account? <Link to="/login" className="font-bold text-[#5C2751] text-2xl  hover:text-white">Login</Link></p>
 
 
+        <div className="">
+            <h3 className="text-3xl font-bold text-center mt-6">Please Register</h3>
+            <form onSubmit={handleRegister} className="card-body md:w-3/4 lg:1/2 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo URL</span>
+                    </label>
+                    <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn text-white text-xl border-none bg-[#5C2751]">Create Account</button>
+                </div>
+            </form>
+            <p className="text-center font-semibold text-lg mt-4">Already have an account? <Link to="/login" className="font-bold text-[#5C2751] text-2xl  hover:text-white">Login</Link></p>
 
 
-                <ToastContainer
+
+
+            <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -93,8 +127,21 @@ const handleRegister = (e) => {
                 theme="colored"
 
             />
-            </div>
-       
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            // transition: Flip
+            />
+        </div>
+
     );
 };
 
